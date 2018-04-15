67d03f1fe9a440b3f87787e129a0f75b439e4d02
Liberato Silva dos Santos <santos.liberato@gmail.com>
2018-04-15 09:55:51 -0500

    example question and answer

--- tltbot.py
+++ tltbot.py
@@ -56,10 +56,10 @@ try:
 			bot.send_audio(chat_id=update.message.chat_id, audio=open("tlt_audios/TLT_v5.1_intro_v1.mp3", "rb"))
 			#sending example question
 			bot.send_message(chat_id=update.message.chat_id, text="Click on \"Play\" to listen to an example question")
-			bot.send_audio(chat_id=update.message.chat_id, audio=open("TLT_v5_1_example_question_v1.mp3", "rb"))
+			bot.send_audio(chat_id=update.message.chat_id, audio=open("tlt_audios/TLT_v5_1_example_question_v1.mp3", "rb"))
 			#sending example answer
 			bot.send_message(chat_id=update.message.chat_id, text="Click on \"Play\" to listen to an example answer")
-			bot.send_audio(chat_id=update.message.chat_id, audio=open("TLT_v5_1_example_answer_v1.mp3", "rb"))
+			bot.send_audio(chat_id=update.message.chat_id, audio=open("tlt_audios/TLT_v5_1_example_answer_v1.mp3", "rb"))
 			#sending test questions
 			bot.send_message(chat_id=update.message.chat_id, text="Now click on \"Play\" to listen to Question 1")
 			bot.send_audio(chat_id=update.message.chat_id, audio=open("tlt_audios/TLT_v5.1_question1_v1.mp3", "rb"))
