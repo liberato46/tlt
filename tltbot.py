41c7941905ec9d459add8f61ad42c82c37665026
Liberato Silva dos Santos <santos.liberato@gmail.com>
2018-04-29 09:19:46 -0500

    next button

--- tltbot.py
+++ tltbot.py
@@ -45,6 +45,7 @@ try:
 		contact_keyboard=KeyboardButton(text="Send my phone number",request_contact=True)
 		custom_keyboard=[[contact_keyboard]]
 		reply_markup=ReplyKeyboardMarkup(custom_keyboard, resize_keyboard=True)
+		bot.send_audio(chat_id=update.message.chat_id, audio=open("tlt_audios/TLT_v5_1_welcome_v1.mp3", "rb"))
 		bot.send_message(chat_id=update.message.chat_id, text=welcome_message,reply_markup=reply_markup)
 
 	def echo(bot, update):
@@ -112,23 +113,25 @@ try:
 				bot.send_message(chat_id=update.message.chat_id, text="(3) Now click on \"Record\" to record your answer to Question "+str(question_answered+1))
 
 	def next_button(bot, update):
-		print("1")
-		bot.edit_message_text(chat_id=update.message.chat_id, text="LALALALALALA", message_id=update.message.message_id)
-		#bot.send_video(chat_id=update.message.chat_id, video=open("tlt_audios/TLT_v5_1_tutorial_v1.mp4", "rb"))
-		bot.send_message(chat_id=update.message.chat_id, text="""Tourism English Language Test – TLT Test
+		message_id=update.callback_query.message.message_id
+		chat_id=update.callback_query.message.chat.id
+
+		bot.send_video(chat_id=update.message.chat_id, video=open("tlt_audios/TLT_v5_1_tutorial_v1.mp4", "rb"))
+		
+		bot.edit_message_text(message_id=message_id, chat_id=chat_id, text="""Tourism English Language Test – TLT Test
 1)	In this test, you will be presented with several scenarios where a hotel front desk staff member responds to a guest.
-2)	Read and listen to the scenarios and what the guest says. Then take on the role of the hotel staff and answer the guest in English.
+2)	Read and listen to the scenarios. Then listen to what the guest says. Take on the role of a front desk staff member and answer the guest in English.
 3)	You should answer the guest in a manner that is appropriate for a hotel front desk work situation.
-4)	Use the voice recording feature in your computer or smart phone to record your answers. Then upload your recorded answer to each question""")
-		print("2")
+4)	Use the voice recording feature in Telegram to record your answer. Then upload your recorded answer to each question""")
+		
 		time.sleep(5)
 		#sending example question
-		bot.send_message(chat_id=update.message.chat_id, text="Click on \"Play\" to listen to an example question and answer")
-		bot.send_audio(chat_id=update.message.chat_id, audio=open("tlt_audios/TLT_v5_1_example_question_and_answer_v1.mp3", "rb"))
+		bot.send_message(chat_id=chat_id, text="Click on \"Play\" to listen to an example question and answer")
+		bot.send_audio(chat_id=chat_id, audio=open("tlt_audios/TLT_v5_1_example_question_and_answer_v1.mp3", "rb"))
 		#sending test questions
-		bot.send_message(chat_id=update.message.chat_id, text="Now click on \"Play\" to listen to Question 1")
-		bot.send_audio(chat_id=update.message.chat_id, audio=open("tlt_audios/TLT_v5.1_question1_v1.mp3", "rb"))
-		bot.send_message(chat_id=update.message.chat_id, text="(3) Now click on \"Record\" to record your answer to Question 1")
+		bot.send_message(chat_id=chat_id, text="Now click on \"Play\" to listen to Question 1")
+		bot.send_audio(chat_id=chat_id, audio=open("tlt_audios/TLT_v5.1_question1_v1.mp3", "rb"))
+		bot.send_message(chat_id=chat_id, text="(3) Now click on \"Record\" to record your answer to Question 1")
 
 	def error(bot, update, error):
 		print("ERROR"+str(update)+" by "+str(error))
