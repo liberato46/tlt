d665220ebbc6f3a9d280b2d20be34d6387cad4a3
Liberato Silva dos Santos <santos.liberato@gmail.com>
2018-04-29 16:58:13 -0500

    adjusting time.sleep method (4)

--- tltbot.py
+++ tltbot.py
@@ -131,11 +131,10 @@ try:
 4)	Use the voice recording feature in Telegram to record your answer. Then upload your recorded answer to each question""")
 
 		bot.send_audio(chat_id=chat_id, audio=open("tlt_audios/TLT_v5.1_intro_v1.mp3", "rb"))
-		
-		time.sleep(40)
+		time.sleep(10)
 
 		bot.send_message(chat_id=chat_id, text="Click on \"Play\" below to listen to an example question and answer")
-		time.sleep(20)
+		
 		#sending example question
 		bot.send_message(chat_id=chat_id, text="""Example Question
 
@@ -147,12 +146,12 @@ You say: Sure. You will see two remote controls on the bedside table. Use the bl
 """)
 		
 		bot.send_audio(chat_id=chat_id, audio=open("tlt_audios/TLT_v5_1_example_question_and_answer_v1.mp3", "rb"))
-		time.sleep(60)
+		time.sleep(20)
 
 		#sending test questions
 		bot.send_message(chat_id=chat_id, text="Now click on \"Play\" to listen to Question 1")
 		bot.send_audio(chat_id=chat_id, audio=open("tlt_audios/TLT_v5.1_question1_v1.mp3", "rb"))
-		time.sleep(30)
+		time.sleep(5)
 		bot.send_message(chat_id=chat_id, text="(3) Now click on \"Record\" to record your answer to Question 1")
 
 	def error(bot, update, error):
